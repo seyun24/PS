402699c81668e424e16601e750930bbf047b6ccb
SEYUN <87846913+seyun24@users.noreply.github.com>
2022-06-21 16:48:11 +0900

    Create 1546.java

---
+++ 자바/1546.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int N=sc.nextInt();
+
+        long sum=0;
+
+        int arr[]=new int[N];
+
+        for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+            sum+=arr[i];
+        }
+        Arrays.sort(arr);
+
+        System.out.println(sum*100.0/arr[N-1]/N);
+
+    }
+}
